eac4373163585c6723c50a9c8cc53d743eeaee95
Esubale Endale <esu1011esu@gmail.com>
2024-11-08 01:17:11 +0300

    code modified after review

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const modal = document.querySelector(".alert-overlay");
-const btnEl = document.querySelectorAll(".btn");
+const alertOverlay = document.querySelector(".alert-overlay");
+const closeBtnEl = document.getElementById("closeBtn");
+const cancelBtnEl = document.getElementById("cancelBtn");
+const continueBtnEl = document.getElementById("continueBtn");
+const dialogBtnEl = document.getElementById("dialogBtn");
 
 const modalToggler = (event) => {
   event.preventDefault();
   modal.classList.toggle("hidden");
 };
 
-btnEl.forEach((el) =>
-  el.addEventListener("click", (event) => modalToggler(event))
-);
+closeBtnEl.addEventListener("click", (event) => modalToggler(event));
+cancelBtnEl.addEventListener("click", (event) => modalToggler(event));
+continueBtnEl.addEventListener("click", (event) => modalToggler(event));
+dialogBtnEl.addEventListener("click", (event) => modalToggler(event));
+
+alertOverlay.addEventListener("click", (e) => {
+  if (e.target.classList.contains("alert-overlay")) modalToggler(e);
+});
