085e2941776d0810a0f93e0a2dc46d0afad4ec3a
Esubale Endale <esu1011esu@gmail.com>
2024-11-08 14:37:16 +0300

    event listners modified

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,27 +1,29 @@
 const alertOverlay = document.querySelector(".alert-overlay");
-const closeBtnEl = document.getElementById("closeBtn");
-const cancelBtnEl = document.getElementById("cancelBtn");
 const continueBtnEl = document.getElementById("continueBtn");
-const dialogBtnEl = document.getElementById("dialogBtn");
+const btnEl = document.querySelectorAll(".btn");
 
 const modalToggler = (event) => {
   event.preventDefault();
+  if (event.target === continueBtnEl)
+    alert("You have deleted the data succesfully");
+
   alertOverlay.classList.toggle("hidden");
 };
 
-closeBtnEl.addEventListener("click", (event) => modalToggler(event));
-cancelBtnEl.addEventListener("click", (event) => modalToggler(event));
-continueBtnEl.addEventListener("click", (event) => modalToggler(event));
-dialogBtnEl.addEventListener("click", (event) => modalToggler(event));
+btnEl.forEach((El) =>
+  El.addEventListener("click", (event) => modalToggler(event))
+);
 
 alertOverlay.addEventListener("click", (e) => {
   if (e.target.classList.contains("alert-overlay")) modalToggler(e);
 });
 
 /*
-const btnEl = document.querySelectorAll(".btn");
+// const closeBtnEl = document.getElementById("closeBtn");
+// const cancelBtnEl = document.getElementById("cancelBtn");
+// const dialogBtnEl = document.getElementById("dialogBtn");
+// closeBtnEl.addEventListener("click", (event) => modalToggler(event));
+// cancelBtnEl.addEventListener("click", (event) => modalToggler(event));
+// dialogBtnEl.addEventListener("click", (event) => modalToggler(event));
 
-btnEl.forEach((El) =>
-  El.addEventListener("click", (event) => modalToggler(event))
-);
 */
